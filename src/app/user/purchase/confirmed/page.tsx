88ceda200a265964ee666514a0c24bc9d0252d75
Ngô Đăng Trường Sa <Ngotruongsa2002@gmail.com>
2024-10-22 16:41:18 +0700

    Cập nhật code cho trang lịch sử mua hàng của user

---
+++ src/app/user/purchase/confirmed/page.tsx
@@ -0,0 +1,19 @@
+import { user } from '@/user';
+import Purchase from '@/components/user/orders/purchase';
+import Confirmed from '@/components/user/orders/confirmed/confirmed';
+
+
+import React from 'react';
+
+const OrderPage = async () => {
+  // const session = await auth();
+
+  return (
+    <>
+      <Purchase></Purchase>
+      <Confirmed></Confirmed>
+    </>
+  );
+};
+
+export default OrderPage;
